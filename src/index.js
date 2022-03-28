2a7406a2572e9816f2c42f79de07b79520ebc670
MoniNeo <moni.gangane@gmail.com>
2022-03-28 15:08:10 +0530

    spread operators of objcet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-const first = [1, 2, 3];
-const second = [4, 5, 6];
-const combined = first.concat(second);
+const first = { name: "Moni" };
+const second = { job: "fr" };
+
+const combined = { ...first, ...second, location: "Pune" };
 console.log(combined);
-const combined1 = [...first, "a", ...second, "b"];
-console.log(combined1);
