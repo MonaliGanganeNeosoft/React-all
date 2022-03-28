86310768fa89e350444baa39faef7caadb0d6278
MoniNeo <moni.gangane@gmail.com>
2022-03-28 14:44:19 +0530

    spread using array

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-const address = {
-  street: "",
-  city: "",
-  country: "",
-};
-const street = address.street;
-const city = address.city;
-const country = address.country;
-
-//object destructure method
-const { street: st, city: ci, country: co } = address;
+const first = [1, 2, 3];
+const second = [4, 5, 6];
+const combined = first.concat(second);
+console.log(combined);
+const combined1 = [...first, "a", ...second, "b"];
+console.log(combined1);
