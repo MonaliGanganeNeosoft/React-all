06da63f994d41838ce22154ae76054a0aac171f9
MoniNeo <moni.gangane@gmail.com>
2022-03-28 15:44:02 +0530

    Named and default exports

--- src/teacher.js
+++ src/teacher.js
@@ -1,5 +1,8 @@
 import { Person } from "./person";
-export class Teacher extends Person {
+
+export function promote() {}
+
+export default class Teacher extends Person {
   constructor(fname, lname, degree) {
     super(fname, lname);
     this.degree = degree;
