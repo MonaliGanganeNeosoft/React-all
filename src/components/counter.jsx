d0f4e4a2952ba241de85d562eab57830f6e27d58
MoniNeo <moni.gangane@gmail.com>
2022-03-28 16:44:23 +0530

    only badge

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,10 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200",
   };
   render() {
     return (
       <>
-        <img src={this.state.imageUrl} alt="" />
         <span className="badge badge-primary">{this.formatCount()}</span>
         <button>Increment</button>
       </>
