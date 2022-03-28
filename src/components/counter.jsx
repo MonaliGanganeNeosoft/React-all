ec7e9983f82078cadc7b2f0a12b1a5c23a9e1405
MoniNeo <moni.gangane@gmail.com>
2022-03-28 17:53:44 +0530

    increment handleclick

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,28 +2,32 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: [],
   };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-    return (
-      <>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </>
-    );
+  handleIncrement() {
+    console.log("Increment click");
   }
   render() {
     return (
       <>
-        {this.state.tags.length === 0 && "pls create a new tags"}
-        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </>
     );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 export default Counter;
