94c9b10d2eb3edbd22c4a9429f7c96fd2b5bdb32
MoniNeo <moni.gangane@gmail.com>
2022-03-28 16:28:40 +0530

    with fragments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 class Counter extends Component {
   render() {
     return (
-      <div>
+      <>
         <h1>hello world</h1>
         <button>Increment</button>
-      </div>
+      </>
     );
   }
 }
