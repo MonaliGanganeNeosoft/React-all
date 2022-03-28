8fe4d2090295eedd24edbada71bced86482a61f4
MoniNeo <moni.gangane@gmail.com>
2022-03-28 14:11:35 +0530

    talk with this keywors function object

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-const jobs = [
-  { id: 1, isActive: true },
-  { id: 2, isActive: true },
-  { id: 3, isActive: false },
-];
-const activeJobs = jobs.filter(function (job) {
-  return job.isActive;
-});
-console.log(activeJobs);
-//arrow function
-const activeJobs1 = jobs.filter((job) => job.isActive);
-console.log(activeJobs1);
+const person = {
+  talk() {
+    console.log("this", this);
+  },
+};
+person.talk();
