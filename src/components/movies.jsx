552e851675d37d47cf27b45a7c5109a8d863a05d
MoniNeo <moni.gangane@gmail.com>
2022-03-29 20:03:11 +0530

    heart first true is done

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
 class Movies extends Component {
   state = {
     movies: getMovies(),
@@ -24,6 +25,7 @@ class Movies extends Component {
               <th>Stock</th>
               <th>Rate</th>
               <th></th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -33,6 +35,9 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like liked={movie.liked} />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
