d8b97d849210396ecd2f9d7b05547ef3e1451a55
MoniNeo <moni.gangane@gmail.com>
2022-03-29 19:22:02 +0530

    decrement btn is done

--- src/App.js
+++ src/App.js
@@ -17,11 +17,19 @@ export class App extends Component {
   componentDidMount() {
     console.log("App - mounted");
   }
+
   handleIncrement = (counter) => {
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
     counters[index] = { ...counter };
     counters[index].value++;
+    this.setState({ counters });
+  };
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
     this.setState({ counters });
   };
   handleReset = () => {
@@ -47,6 +55,7 @@ export class App extends Component {
             counters={this.state.counters}
             onReset={this.handleReset}
             onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
             onDelete={this.handleDelete}
           />
         </main>
