4c01f80fb8415cf5ab743780aca00c8aba9c5922
MoniNeo <moni.gangane@gmail.com>
2022-03-29 13:42:09 +0530

    count change to value name

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: this.props.value,
+    value: this.props.value,
   };
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
@@ -24,13 +24,13 @@ class Counter extends Component {
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 export default Counter;
