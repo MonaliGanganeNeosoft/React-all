c8251ae1fe128d7432544c4cf8d8f53f59276f18
MoniNeo <moni.gangane@gmail.com>
2022-03-30 11:39:22 +0530

    currentPage is loded when we clicked

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import _ from "lodash"; //for underscore
 const Pagination = (props) => {
-  const { itemsCount, pageSize } = props;
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+  console.log(currentPage);
   const pagesCount = Math.ceil(itemsCount / pageSize);
   //[1,2,3].map()
   //[1 ... pagesCount].map()
-  console.log(pagesCount);
+  //console.log(pagesCount);
   if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
   return (
     <nav>
       <ul className="pagination">
         {pages.map((page) => (
-          <li key={page} className="page-item">
-            <a className="page-link" onClick={() => props.onPageChange(page)}>
+          <li key={page} className="page-item ">
+            <a className="page-link" onClick={() => onPageChange(page)}>
               {page}
             </a>
           </li>
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,6 +5,7 @@ import Pagination from "./common/pagination";
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    currentPage: 1,
     pageSize: 4,
   };
   handleDelete = (movie) => {
@@ -19,10 +20,11 @@ class Movies extends Component {
     this.setState({ movies });
   };
   handlePageChange = (page) => {
-    console.log(page);
+    this.setState({ currentPage: page });
   };
   render() {
     const { length: count } = this.state.movies;
+    const { pageSize, currentPage } = this.state;
     if (count === 0) {
       return <p>There are no movies in the database</p>;
     }
@@ -67,7 +69,8 @@ class Movies extends Component {
         </table>
         <Pagination
           itemsCount={count}
-          pageSize={this.state.pageSize}
+          pageSize={pageSize}
+          currentPage={currentPage}
           onPageChange={this.handlePageChange}
         />
       </>
