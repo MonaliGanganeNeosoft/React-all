ed172ae24e66e87fc5b285eea84311968e545079
MoniNeo <moni.gangane@gmail.com>
2022-04-01 21:39:25 +0530

    moviesForm of id is working

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,6 +1,16 @@
 import React from "react";
-const movieForm = () => {
-  return <h1>Movieform</h1>;
+const movieForm = ({ match, history }) => {
+  return (
+    <>
+      <h1>Movieform {match.params.id}</h1>
+      <button
+        className="btn btn-primary"
+        onClick={() => history.push("/movies")}
+      >
+        Save
+      </button>
+    </>
+  );
 };
 
 export default movieForm;
