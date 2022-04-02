9d551f24c827cee1620ea30fbbca37aa90807657
MoniNeo <moni.gangane@gmail.com>
2022-04-02 14:16:58 +0530

    all input render method done

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
-import Input from "./common/input";
 import Form from "./common/form";
+
 class LoginForm extends Form {
   state = {
     data: { username: "", password: "" },
@@ -20,25 +20,12 @@ class LoginForm extends Form {
   };
 
   render() {
-    const { data, errors } = this.state;
     return (
       <>
         <h1>Login form</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            value={data.username}
-            label="Username"
-            onChange={this.handleChange}
-            error={errors.username}
-          />
-          <Input
-            name="password"
-            value={data.password}
-            label="Password"
-            onChange={this.handleChange}
-            error={errors.password}
-          />
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", Password)}
           {this.renderButton("Login")}
         </form>
       </>
