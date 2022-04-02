1b5fa9d01814c35eee5393e6bcb573e7051c3ecc
MoniNeo <moni.gangane@gmail.com>
2022-04-02 09:49:54 +0530

    routing added to loginForm done

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -28,6 +28,9 @@ const NavBar = () => {
           <NavLink className="nav-link" to="/rentals">
             Rentals
           </NavLink>
+          <NavLink className="nav-link" to="/login">
+            Login
+          </NavLink>
         </div>
       </div>
     </nav>
