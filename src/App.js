a7a59b5e7d581bdc1107039ebb8bfbb0cc8bb47a
MoniNeo <moni.gangane@gmail.com>
2022-04-04 16:44:40 +0530

    extracting a reusable http service

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React, { Component } from "react";
-import axios from "axios";
+import http from "./services/httpService";
 import "./App.css";
-
-axios.interceptors.response.use(null, (error) => {
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500;
-
-  if (!expectedError) {
-    console.log("logging the error", error);
-    alert("an unexpected error occured ");
-  }
-
-  return Promise.reject(error);
-});
 
 const apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
 
@@ -25,13 +11,13 @@ class App extends Component {
 
   async componentDidMount() {
     //pending >resolved(success) or rejected(failure)
-    const { data: posts } = await axios.get(apiEndpoint);
+    const { data: posts } = await http.get(apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await axios.post(apiEndpoint, obj);
+    const { data: post } = await http.post(apiEndpoint, obj);
 
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
@@ -39,9 +25,8 @@ class App extends Component {
 
   handleUpdate = async (post) => {
     post.title = "Updated";
-    // axios.patch(apiEndpoint + "/" + post.id, { title: post.title });
 
-    await axios.put(apiEndpoint + "/" + post.id, post);
+    await http.put(apiEndpoint + "/" + post.id, post);
 
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
@@ -54,7 +39,7 @@ class App extends Component {
     const posts = this.state.posts.filter((p) => p.id !== post.id);
     this.setState({ posts });
     try {
-      await axios.delete(apiEndpoint + "/" + post.id);
+      await http.delete(apiEndpoint + "/" + post.id);
     } catch (ex) {
       if (ex.response && ex.response.status === 404) {
         alert("This post has alreay been deleted");
